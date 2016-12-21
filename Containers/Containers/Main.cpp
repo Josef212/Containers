08948f962da82cdeb37c022adcb543e4d4a88e58
Josef21296 <jcasanovasv@hotmail.com>
2016-12-21 09:56:29 +0100

    Main vector test

--- Containers/Containers/Main.cpp
+++ Containers/Containers/Main.cpp
@@ -2,12 +2,21 @@
 
 #include "Globals.h"
 #include "String.h"
+#include "Vector.h"
 #include "JQuadTree.h"
 #include "GameObject.h"
 
 int main(int argc, char** argv)
 {
 	LOG("Hi!");
+
+	Vector<int> vec;
+	Vector<int> vec2(15);
+	Vector<int> vec3(vec2);
+
+	LOG("Vec1: %d-%d.", vec.capacity(), vec.length());
+	LOG("Vec2: %d-%d.", vec2.capacity(), vec2.length());
+	LOG("Vec3: %d-%d.", vec3.capacity(), vec3.length());
 
 	system("PAUSE");
 
